482400cb82c2250ba944fda2d3a346b1517fc5be
krish2004-creator <vkrishnaveni299@gmail.com>
2025-03-15 22:38:08 +0530

    15/03/2025

---
+++ src/Problems/ReverseArray.java
@@ -0,0 +1,13 @@
+package Problems;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr={23,4,78,89,67};
+        for(int i= arr.length-1;i>0;i--){
+            System.out.println(arr[i]);
+
+        }
+
+
+    }
+}
