98bc1256cf473d40fda9d69374058ae018c213e2
krish2004-creator <vkrishnaveni299@gmail.com>
2025-03-25 09:02:40 +0530

    25/03/2024

---
+++ src/Problems/FirstRepeatingElement.java
@@ -0,0 +1,20 @@
+package Problems;
+import java.util.HashSet;
+public class FirstRepeatingElement {
+        public static void main(String[] args) {
+            int[] arr = {10, 5, 3, 4, 3, 5, 6};
+            HashSet<Integer> set = new HashSet<>();
+            int firstRepeating = -1;
+
+            for (int i = arr.length - 1; i >= 0; i--) {
+                if (set.contains(arr[i])) {
+                    firstRepeating = arr[i];
+                } else {
+                    set.add(arr[i]);
+                }
+            }
+            System.out.println("First Repeating Element:" + firstRepeating);
+        }
+    }
+
+
