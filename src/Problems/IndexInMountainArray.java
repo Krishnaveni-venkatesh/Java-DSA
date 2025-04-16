654a1e70798f9b0779bc5ec33b097a30234b9d8c
krish2004-creator <vkrishnaveni299@gmail.com>
2025-04-16 19:29:55 +0530

    16/04/2025

---
+++ src/Problems/IndexInMountainArray.java
@@ -0,0 +1,20 @@
+package Problems;
+
+public class IndexInMountainArray {
+    public int MountainArray(int[] arr){
+        int i=0;
+        while(arr[i] < arr[i+1]){
+            i++;
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        IndexInMountainArray obj=new IndexInMountainArray();
+        int[] mountainArr = {1, 3, 5, 7, 6, 4, 2};
+        int peakIndex = obj.MountainArray(mountainArr);
+        System.out.println(peakIndex);
+
+
+    }
+}
