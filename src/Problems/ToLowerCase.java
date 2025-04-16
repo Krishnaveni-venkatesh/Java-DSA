7296edb377aefff4fe4ec0e4835da6b75714c446
krish2004-creator <vkrishnaveni299@gmail.com>
2025-04-16 18:55:13 +0530

    16/04/2025

--- src/Problems/ToLowerCase.java
+++ src/Problems/ToLowerCase.java
@@ -1,5 +1,20 @@
 package Problems;
 
-public class UpperCase {
+public class ToLowerCase {
+    public String toLowerCase(String s){
+        char[]c=s.toCharArray();
+        for(int i=0;i<c.length;i++){
+            if(c[i]>=65 && c[i]<=90){ //using ascii values
+                c[i]=(char) (c[i]+32);
+            }
+        }
+        return String.valueOf(c);
+    }
+
+    public static void main(String[] args) {
+        ToLowerCase obj = new ToLowerCase();
+        String result = obj.toLowerCase("Hello");
+        System.out.println(result);
+    }
 
 }
