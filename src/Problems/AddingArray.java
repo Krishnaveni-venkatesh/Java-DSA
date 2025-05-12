2cfa392321209be35a5d8ae938bb325744fd2ade
krish2004-creator <vkrishnaveni299@gmail.com>
2025-05-12 07:47:28 +0530

    12/05/2025

---
+++ src/Problems/AddingArray.java
@@ -0,0 +1,41 @@
+//input: num=[1,2,0,0] k=[3,4]
+//output:[1,2,3,4]
+//explanation=1200+34=1234
+package Problems;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class AddingArray {
+    public static List<Integer> addToArrayForm(int[] num, int[] k) {
+        List<Integer> result = new ArrayList<>();
+        int i = num.length - 1;
+        int j = k.length - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int n1 = (i >= 0) ? num[i] : 0;
+            int n2 = (j >= 0) ? k[j] : 0;
+
+            int sum = n1 + n2 + carry;
+            result.add(sum % 10);
+            carry = sum / 10;
+
+            i--;
+            j--;
+        }
+
+
+        Collections.reverse(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] num = {1, 2, 0, 0};
+        int[] k = {3, 4};
+
+        List<Integer> output = addToArrayForm(num, k);
+        System.out.println(output);
+    }
+}
+
---
+++ src/Problems/Findfirstandlast.java
@@ -0,0 +1,70 @@
+package Problems;
+import java.util.ArrayList;
+//input={5, 7, 7, 8, 8, 10};,target=8  returning index of first and last position of target element in sorted array
+//output =[3,4]
+
+public class Findfirstandlast {
+    public static ArrayList<Integer> searchRange(int[] nums, int target) {
+        ArrayList<Integer> result = new ArrayList<>();
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+
+        result.add(first);
+        result.add(last);
+
+        return result;
+    }
+
+    private static int findFirst(int[] nums, int target) {
+        int index = -1;
+        int start = 0, end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] >= target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+
+            if (nums[mid] == target) {
+                index = mid;
+            }
+        }
+
+        return index;
+    }
+
+    private static int findLast(int[] nums, int target) {
+        int index = -1;
+        int start = 0, end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+
+            if (nums[mid] == target) {
+                index = mid;
+            }
+        }
+
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+
+        ArrayList<Integer> result = searchRange(nums, target);
+
+        System.out.println( result);
+    }
+}
+
+
