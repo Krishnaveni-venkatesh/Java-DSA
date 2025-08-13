d858709661809128c79b201ca35b4ac87f879f73
krish2004-creator <vkrishnaveni299@gmail.com>
2025-08-13 21:56:20 +0530

    13/08/2025

---
+++ src/Problems/CeilingNumber.java
@@ -0,0 +1,27 @@
+package Problems;
+
+public class CeilingNumber {
+    static int ceilingNumber(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return arr[mid];
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+
+            } else {
+                end = mid - 1;
+            }
+
+        }
+        return arr[start];
+    }
+    public static void main(String[]args){
+        int[] arr={2,3,5,14,16,18};
+        int target =15;
+        System.out.println(ceilingNumber(arr,target));
+    }
+}
