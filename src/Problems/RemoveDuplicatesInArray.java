1c21c5b6c18cdc440790128513f0444f212ba38d
krish2004-creator <vkrishnaveni299@gmail.com>
2026-02-11 12:25:03 +0530

    Array Problems

---
+++ src/Problems/RemoveDuplicatesInArray.java
@@ -0,0 +1,15 @@
+package Problems;
+
+import java.util.ArrayList;
+
+public class RemoveDuplicatesInArray {
+    public static int[] remo(int[] arr){
+        ArrayList<Integer> res=new ArrayList<>();
+        int j=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]=arr[j]){
+
+            }
+        }
+    }
+}
