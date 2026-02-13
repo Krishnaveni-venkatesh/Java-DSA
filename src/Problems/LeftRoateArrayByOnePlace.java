7c2d7faba914dc85a395a4415dc33ee05a30ca3e
krish2004-creator <vkrishnaveni299@gmail.com>
2026-02-13 20:53:34 +0530

    Array Problems

---
+++ src/Problems/LeftRoateArrayByOnePlace.java
@@ -0,0 +1,21 @@
+package Problems;
+
+import java.util.Arrays;
+
+public class LeftRoateArrayByOnePlace {
+    public static int[] leftRotate(int[] arr){
+        int temp=arr[0];
+        for(int i=1;i<arr.length;i++){
+            arr[i-1]=arr[i];
+
+        }
+        arr[arr.length-1]=temp;
+
+
+        return arr;
+    }
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5};
+        System.out.println(Arrays.toString(leftRotate(arr)));
+    }
+}
--- src/Problems/RemoveDuplicatesInArray.java
+++ src/Problems/RemoveDuplicatesInArray.java
@@ -1,15 +1,30 @@
 package Problems;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class RemoveDuplicatesInArray {
-    public static int[] remo(int[] arr){
-        ArrayList<Integer> res=new ArrayList<>();
-        int j=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]=arr[j]){
-
+    public static int remo(int[] arr){
+        int i=0;
+        for(int j=1;j<arr.length;j++){
+            if(arr[j]!=arr[i]){
+                int temp=arr[i+1];
+                arr[i+1]=arr[j];
+                arr[j]=temp;
+                i++;
             }
+
+        }
+        return i+1;
+
+    }
+    public static void main(String[] args){
+        int[] arr={1,1,2,3,5,8,8};
+        int newLength=remo(arr);
+        for(int k=0;k<newLength;k++){
+            System.out.print(arr[k]+" ");
+        }
+
         }
     }
-}
+
